65f57b8f994f50ad4b6822e90672ed1a516e9081
liyunghao <liyunghao@gmail.com>
2022-06-02 03:21:47 +0800

    feat: add route interface

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { Route, Routes, BrowserRouter, Navigate, Outlet } from 'react-router-dom';
+import Login from './components/login'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Auth = () => {
+  const user=localStorage.getItem('token')
+  if (token) {
+    return true;
+  }
+  return false;
+}
+
+function AuthRoute () {
+  const auth = Auth()
+  return auth ? <Outlet/> : <Navigate to="/login"/>
+}
+function PublicRoute () {
+  const auth = Auth()
+  return auth ? <Navigate to="Dashboard" /> : <Outlet/>
+}
+class App extends Component {
+	construstor(props) {
+		super(props);
+	}
+	
+	this.state = {
+		token: null
+	}
+	
+	render() {
+		return (
+      <BrowserRouter>
+        <Routes>
+          /** Wrap all route here **/ 
+          <Route path="/" element = {<AuthRoute/>}>
+            
+          </Route>
+          
+          // login route
+          <Route path="/login" element={<PublicRoute>}>
+            <Route path="/login" element={<Login/>} />  
+          </Route>
+        </Routes>
+      </BrowserRouter>
+
+
+
+		)
+	}
 }
 
 export default App;
---
+++ src/components/login.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react'
+import {useNavigate} from 'react-router-dom'
+
+class Login extends Component {
+  construtor(props) {
+    super(props)
+  }
+  
+  render() {
+    const
+    return (
+
+
+    )
+  }
+
+
+}
