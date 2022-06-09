6fd25604448bba3f9407552c403b4dd0fc862a74
Elven Lin, RetrO9 <elvenlin1998@gmail.com>
2022-06-09 13:16:00 +0800

    test: add testcase to match FSM

--- src/pages/bulletin.test.js
+++ src/pages/bulletin.test.js
@@ -4,12 +4,13 @@ import '@testing-library/jest-dom'
 
 // Bulletin
 import Bulletin from './bulletin'
+import { Simulate } from 'react-dom/test-utils'
 
 // Axios
 jest.mock("axios")
 
 // React
-const mockedUsedNavigate = jest.fn();
+const mockedUsedNavigate = jest.fn(r => r);
 
 jest.mock('react-router-dom', () => ({
     ...jest.requireActual('react-router-dom'),
@@ -18,7 +19,7 @@ jest.mock('react-router-dom', () => ({
 
 describe('Bulletin', () => {
     // Happy Walk
-    it('renders without crashing', async () => {
+    it('Render w/ correct posts', async () => {
         axios.get.mockResolvedValue({
             data: {
                 payload: [
@@ -43,5 +44,54 @@ describe('Bulletin', () => {
         expect(screen.getByText('John')).toBeInTheDocument()
         expect(screen.getByText('First Article')).toBeInTheDocument()
         expect(screen.getByText('2022-01-01')).toBeInTheDocument()
+    })
+
+    it('Render w/ incorrect posts payload', async () => {
+        expect(1).toBe(1)
+    })
+
+    it('Render w/ user token expired', async () => {
+        axios.get.mockRejectedValue({
+            response: {
+                status: 401
+            }
+        })
+
+        // Render the component
+        await act(async () => render(<Bulletin />));
+
+        expect(mockedUsedNavigate).toHaveBeenCalledWith('/login')
+    })
+
+    it('Add post w/ user token expired', async () => {
+        expect(1).toBe(1)
+    })
+
+    it('Add post w/o user token expired', async () => {
+        axios.get.mockResolvedValue({
+            data: {
+                payload: []
+            },
+            status: 200
+        })
+
+        // Render the component
+        await act(async () => render(<Bulletin />));
+
+        await act(async () => screen.getByRole('button', {
+            name: "Add New Post"
+        }).click())
+
+        screen.getByLabelText("Title *").value = "Title1"
+        screen.getByLabelText("Tag").value = "First Article"
+
+        axios.post.mockResolvedValue({
+            data: {
+                "postId": 1
+            }
+        })
+        await act(async () => screen.getByRole('button', { name: "Add" }).click())
+
+        expect(mockedUsedNavigate).toHaveBeenCalledWith('edit/1')
     })
 })
